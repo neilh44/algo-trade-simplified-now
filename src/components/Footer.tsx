f7e790f82d1ba77a48759b66a57383e6f7b4585d
neilh44 <nilesh.hanotia@outlook.com>
2025-07-11 17:45:32 +0530

    Update Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -62,7 +62,7 @@ const Footer = ({ id }: FooterProps) => {
               </div>
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-orange-400" />
-                <span className="text-gray-300">Mumbai, Maharashtra, India</span>
+                <span className="text-gray-300">TF1, JIVABHAI MARKET, KALOl, GANDHINAGAR, GUJARAT, INDIA</span>
               </div>
             </div>
 
