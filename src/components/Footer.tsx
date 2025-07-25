19d1147f7c35c0a3e9958458e0f9009ca28f32b7
Nilesh Hanotia <nilesh.hanotia@outlook.com>
2025-07-25 15:06:16 +0530

    update email address

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,7 +83,7 @@ const Footer = ({ id }: FooterProps) => {
             <div className="space-y-4">
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-400" />
-                <span className="text-gray-300">support@automatealgos.com</span>
+                <span className="text-gray-300">support@automatealgos.in</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-green-400" />
